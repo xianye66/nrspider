85c3c0c2e6fd55cb0bda24d50638701bef86155f
kevin-zx <519754821@qq.com>
2017-05-15 23:29:11 +0800

    日常提交

--- store/yicai_store.go
+++ store/yicai_store.go
@@ -6,13 +6,18 @@ import (
 	"html/template"
 	"github.com/kevin-zx/go-util/errorUtil"
 	"os"
+
+	"github.com/snluu/uuid"
 )
 
 func unescaped (x string) interface{} { return template.HTML(x) }
 
 func ArticleStoreAsFile(art article.ArticleInfo){
+	fileName  :=  uuid.Rand().Hex()+".html"
+	f, err := os.OpenFile(fileName, os.O_CREATE, 0666)
 	t := template.New("article.html").Funcs(template.FuncMap{"unescaped": unescaped})
-	t,err := t.ParseFiles("template/article.html")
+	t,err = t.ParseFiles("template/article.html")
 	errorUtil.CheckErrorExit(err)
-	errorUtil.CheckErrorExit(t.Execute(os.Stdout,art))
+
+	errorUtil.CheckErrorExit(t.Execute(f,art))
 }
